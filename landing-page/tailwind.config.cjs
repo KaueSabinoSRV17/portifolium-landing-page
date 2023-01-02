f0818f8a0bcdf2e7257471182de7d042ad4aa59f
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-01-02 23:55:35 +0000

    feat: default styles

--- landing-page/tailwind.config.cjs
+++ landing-page/tailwind.config.cjs
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "background-black": "#22212C",
+        "card-grey": "#302F3D",
+        "text-grey": "#837E9F",
+        "image-border-green": "#00FF00",
+        "stacks-pink": "#CB92B1"
+      }
+    },
   },
   plugins: [],
 }
