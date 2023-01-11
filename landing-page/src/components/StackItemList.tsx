b0726fb46a50306380e2f94570f5aaf1ad7fdd8c
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-01-11 00:09:25 +0000

    feat: new stacks

--- landing-page/src/components/StackItemList.tsx
+++ landing-page/src/components/StackItemList.tsx
@@ -6,13 +6,16 @@ export function StackItemList() {
         'Java',
         'Spring',
         'Javascript',
+        'Typescript',
+        'Tailwind',
+        'Git',
         'React',
         'Cypress',
         'Docker'
     ]
 
     return (
-        <ul className="grid grid-cols-3 gap-[15px]">
+        <ul className="grid grid-cols-3 gap-[15px] mx-[10px]">
             {stackNames.map(name => <li className="bg-stacks-pink rounded-[30px] text-xs text-black text-center font-bold py-[6px] px-[7px]">{name.toUpperCase()}</li>)}
         </ul>
     )
